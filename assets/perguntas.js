33cb022a77976451bf4bf1ec1cb3589cb551ce1f
Evelynvitoria12 <evevitoria1004@gmail.com>
2025-07-07 11:36:00 -0300

    hgdfwfd

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    geografia
+    maior oceano do mundo?
+    Pacífico
+)
+
+criaCartao(
+    ciências 
+    maior osso do corpo humano?
+    fêmur
+)
+
+criaCartao(
+    ciências
+    menor osso do corpo humano?
+    estribo
+)
+
